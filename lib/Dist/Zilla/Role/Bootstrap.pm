dced179abd4764ea4fec16a6f5d5e507ff99cb03
Kent Fredric <kentfredric@gmail.com>
2017-03-03 20:17:07 +1300

    Build results of c802df2 (on master)

--- lib/Dist/Zilla/Role/Bootstrap.pm
+++ lib/Dist/Zilla/Role/Bootstrap.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Role::Bootstrap;
 
-our $VERSION = '1.001004';
+our $VERSION = '1.001005';
 
 # ABSTRACT: Shared logic for bootstrap things.
 
@@ -239,7 +239,7 @@ Dist::Zilla::Role::Bootstrap - Shared logic for bootstrap things.
 
 =head1 VERSION
 
-version 1.001004
+version 1.001005
 
 =head1 SYNOPSIS
 
